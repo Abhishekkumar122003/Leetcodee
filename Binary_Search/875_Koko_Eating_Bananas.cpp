420c05f8eb2145fc62e8a608393089c892e95556
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-07 19:23:42 +0530

    i try to solve this by brute force method

--- Binary_Search/875_Koko_Eating_Bananas.cpp
+++ Binary_Search/875_Koko_Eating_Bananas.cpp
@@ -1,1 +1,13 @@
 //brute force method
+
+for(int speed =1; speed<INT_MAX; speed++){
+    int time_taken=0;
+    //for accessing the piles
+    for(int i=0; i<n; i++){
+        int Bananas = piles[i];
+        time_taken  += ceil(Bananas/speed);
+    }
+    if(time_taken <= givrn_time){
+        return speed;
+    }
+}
