c6106066c41cec6dd06ce3080f34ec9886fbe95e
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-07 19:27:31 +0530

    I doo change the code little bit so that i can save that one extra veriable space called Bananas. i just doo time_take+=piles[i]/speed;

--- Binary_Search/875_Koko_Eating_Bananas.cpp
+++ Binary_Search/875_Koko_Eating_Bananas.cpp
@@ -4,8 +4,10 @@ for(int speed =1; speed<INT_MAX; speed++){
     int time_taken=0;
     //for accessing the piles
     for(int i=0; i<n; i++){
-        int Bananas = piles[i];
-        time_taken  += ceil(Bananas/speed);
+        // int Bananas = piles[i];
+        // time_taken  += ceil(Bananas/speed);
+        // whithout using that extra space called Bananas instead just doo 
+        time_take += piles[i]/speed; // that way you saved one variable space
     }
     if(time_taken <= givrn_time){
         return speed;
