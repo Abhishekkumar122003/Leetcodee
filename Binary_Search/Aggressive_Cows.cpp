dc02e7c9753e44c837b330239f57378a4f58927e
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-23 21:21:55 +0530

    .

--- Binary_Search/Aggressive_Cows.cpp
+++ Binary_Search/Aggressive_Cows.cpp
@@ -2,17 +2,17 @@ class Solution {
   public:
     int aggressiveCows(vector<int> &arr, int k) {
         // code here
-        long long start=1 , end , ans;
+        long long left=1 , right , ans;
         int n = arr.size();
         
         // first Sort the given UnSorted Array
-        sort(arr.begin() , arr.end());
+        sort(arr.begin() , arr.right());
         
-        end = arr[n-1] - arr[0];
+        right = arr[n-1] - arr[0];
         
         // Applying the logic of binary search
-        while(start <= end){
-            long long mid = start + (end - start)/2;
+        while(left <= right){
+            long long mid = left + (right - left)/2;
             long long Pos_of_Cow = arr[0] , Num_of_Cow=1;
             
             for(int i=1; i<n; i++){
@@ -25,9 +25,9 @@ class Solution {
               //check is the number of Cow 
               if(Num_of_Cow >= k){
                   ans = mid;
-                  start = mid +1;
+                  left = mid +1;
               }else{
-                  end = mid -1;
+                  right = mid -1;
               }
         }
         return ans;
